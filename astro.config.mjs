7b78cf432c31b8aa1ddbd3158ca064afa39cf943
Patrick Mahinge <109906110+affilirator@users.noreply.github.com>
2025-07-09 14:46:03 +0300

    site data added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,21 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fx.kenyaforexfirm.com',
+
   integrations: [
     tailwind(),
     mdx(),
     sitemap()
   ],
+
   server: {
     host: '0.0.0.0'
-  }
+  },
+
+  adapter: cloudflare()
 });
