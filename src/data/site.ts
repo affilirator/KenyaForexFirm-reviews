a04d08196a1478e21242bfc79e539d94ae2174cd
Patrick Mahinge <109906110+affilirator@users.noreply.github.com>
2025-07-09 13:58:10 +0300

    site data added

---
+++ src/data/site.ts
@@ -0,0 +1,19 @@
+export const site = {
+    name: 'Kenya Forex Firm Reviews',
+    description: 'Kenya Forex Firm brings you all the reviews that matter from the forex trading industry.',
+    url: import.meta.env.SITE,
+    currentYear: new Date().getFullYear(),
+    address: {
+        emailAddress: "support@kenyaforexfirm.com",
+        city: "Nairobi",
+        state: "Nairobi",
+        country: "Kenya",
+        stateAbbrv: "KE",
+        postalCode: "77001",
+        streetAddress: "123 Industrial Drive",
+        phone: "+254710251380",
+    }
+
+
+
+}
