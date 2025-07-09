4d0d2e2d03e0ce2d9f28842973226254cc42509c
Patrick Mahinge <109906110+affilirator@users.noreply.github.com>
2025-07-09 17:25:34 +0300

    site data added

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,11 +1,20 @@
-const siteUrl = 'https://fx.kenyaforexfirm.com';
-const siteName = "Kenya Forex Firm";
+// If you intended to use import.meta.env.SITE as a fallback, consider:
+const siteUrl = import.meta.env.SITE || "https://fx.kenyaforexfirm.com";
+const organization = 
+    {
+        name: "Kenya Forex Firm",
+        url: "https://kenyaforexfirm.com",
+    }
+
+
+
 export const site = {
     name: 'Best Forex Broker Reviews in Kenya',
-    siteName: siteName,
+    siteName: organization.name,
+    schemaId: `${siteUrl}/#Website`,
     audience: "Forex Traders",
     description: 'Kenya Forex Firm brings you all the reviews that matter from the forex trading industry.',
-    url: import.meta.env.SITE,
+    url: siteUrl,
     currentYear: new Date().getFullYear(),
     canonicalURL: window.location.href,
     logoPath: "/favicon.svg",
@@ -18,20 +27,23 @@ export const site = {
         },
 
 ],
-
-
     address: {
         email: "support@kenyaforexfirm.com",
         city: "Nairobi",
         addressLocality: "Nairobi",
-
         state: "Nairobi",
         country: "Kenya",
         countryShortName: "KE",
         postalCode: "77001",
         streetAddress: "123 Industrial Drive",
         phone: "+254710251380",
-    }
+    },
+    organization: {
+        name: organization.name,
+        url: organization.url,
+        id: `${organization.url}/#Organization`
+    },
+    //org: organization,
     
 }
 //export const email= site.address.emailAddress
