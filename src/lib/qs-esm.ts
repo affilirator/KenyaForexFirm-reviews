6e00f69e0b5e3e231e291522b3e8492d2c536d53
default <patohkihara@gmail.com>
2025-07-09 19:31:11 +0000

    Added Where to Query

--- src/lib/qs-esm.ts
+++ src/lib/qs-esm.ts
@@ -1,19 +1,36 @@
 //import * as qs from 'qs-esm'
 //   'https://localhost:3000/api/posts?select[color]=true&select[group][number]=true'
 import { stringify } from 'qs-esm'
+
 const PAYLOAD_API_URL = "https://api.kenyaforexfirm.com"
+
+const populate = {
+  tradingPlatforms: {
+    name: true,
+    slug: true
+  },
+  regulation: {
+    name: true,
+    shortName: true,
+    }
+};
+
+const query = {
+  status: {
+    equals: 'published'
+}
+}
 
 const select = {
   brokerName: true,
   quickVerdict: true,
   brokerRating: true,
   minDeposit: true,
-  regulation: [
-    {
-      name: true,
-      shortName: true,
-    }
-  ]
+  regulation: true,
+  
+ 
+  
+  
   // This query could be much more complex
   // and QS would handle it beautifully
 }
@@ -22,10 +39,26 @@ const select = {
 export const getReviews = async () => {
   const stringifiedQuery = stringify(
     {
-      select, // ensure that `qs` adds the `select` property, too!
+      //populate,
+      select,
+      where: query,
+      //depth:2,
+      sort: '-brokerRating',
+      //status: 'published',
     },
-    { addQueryPrefix: true },
+    { addQueryPrefix: true,
+    // allowDots: true,
+      
+
+      //allowDots: true,
+      //encodeValuesOnly: true,
+      //arrayFormat: 'repeat', // encode: false,
+     
+     },
+     
+    
   )
+  console.log("The stringified query is:", stringifiedQuery)
   const response = await fetch(`${PAYLOAD_API_URL}/api/broker-reviews${stringifiedQuery}`)
   const reviews = await response.json()
   return reviews
