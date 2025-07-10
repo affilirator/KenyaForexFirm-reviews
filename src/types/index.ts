59c523874dc55b3305f2345ea4755cbfbf7edb6a
default <patohkihara@gmail.com>
2025-07-10 00:00:06 +0000

    Added Where to Query

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-interface Regulation {
+interface BrokerRegulation {
   name: string;
   shortName: string;
   country: string;
@@ -8,8 +8,48 @@ export interface IndexBrokerProps {
   name: string;
   rating: number;
   features: string[];
-  regulation: Regulation[];
+  regulation: BrokerRegulation[];
   minDeposit: number;
   spread: string;
   logo: string;
+}
+
+export type AccountType = {
+    minDeposit: number;
+    accountName: string;
+
+}
+
+export type Regulation = {
+  name: string;
+  shortName: string;
+  country: string;
+  id: string;
+}
+
+export type BrokerReview = {
+  id: string;
+  brokerName: string;
+  maxLeverage: string;
+  brokerRating: number;
+  account?: AccountType[];
+  slug: string;
+  features?: string[];
+  regulation?: Array<Regulation> | Regulation[];
+  minDeposit?: number;
+  spread?: string;
+  logo?: string;
+}
+
+export type BrokerProps = {
+  id: string;
+  brokerName: string;
+  maxLeverage: string;
+  brokerRating: number;
+  slug: string;
+  features?: string[];
+  regulation?: Array<Regulation> | Regulation[];
+  minDeposit?: number;
+  spread?: string;
+  logo?: string;
 }
