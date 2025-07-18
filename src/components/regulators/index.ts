dc9976eccedeb312bdcd5546ab68f4a7f5c680dc
default <patohkihara@gmail.com>
2025-07-18 00:40:11 +0000

    Added ExpertiseOptions

---
+++ src/components/regulators/index.ts
@@ -0,0 +1,12 @@
+// Export all regulator components
+export { default as RegulatorPage } from './RegulatorPage.astro';
+export { default as RegulatorHero } from './RegulatorHero.astro';
+export { default as RegulatorSection } from './RegulatorSection.astro';
+export { default as RegulatorRequirements } from './RegulatorRequirements.astro';
+export { default as RegulatorBenefits } from './RegulatorBenefits.astro';
+export { default as RegulatorComparison } from './RegulatorComparison.astro';
+export { default as RegulatorFAQ } from './RegulatorFAQ.astro';
+export { default as RegulatorConclusion } from './RegulatorConclusion.astro';
+export { default as RegulatorRelatedContent } from './RegulatorRelatedContent.astro';
+export { default as RegulatorEEAT } from './RegulatorEEAT.astro';
+export { default as RichTextRenderer } from './RichTextRenderer.astro';
