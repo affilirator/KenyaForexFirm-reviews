afb37cd096c6e206cf985a922a67cdefd80a83d8
Coder <coder@mahinge.com>
2025-09-21 06:18:08 +0000

    User Password

---
+++ src/lib/review-criteria/safety-regulation.ts
@@ -0,0 +1,8 @@
+const apiUrl = 'https://fx.mahinge.com/api/review-criteria/68c8cbd97ad42da58a6fb119?depth=2&draft=false&locale=undefined&trash=false'
+
+
+export async function getSafetyRegulation () {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data;
+}
