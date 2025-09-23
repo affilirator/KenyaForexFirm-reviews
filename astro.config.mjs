03a525458473f88e69e9ba2d6f6b35eac36c60ec
Coder <coder@mahinge.com>
2025-09-23 04:31:13 +0000

    Update RichText.tsx add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
   },
    image: {
     service: {
-      entrypoint:  'astro/assets/services/sharp',
+      //entrypoint:  'astro/assets/services/sharp',
       config: {
         // ... service-specific config. Optional.
       }
