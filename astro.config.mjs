263090f2bc300deed39b3557fb50519cdf5444ea
Coder <coder@mahinge.com>
2025-09-28 03:00:53 +0000

    BaseLayout Flex Flex-col

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
