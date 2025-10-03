9eef535209f9cdfc3bfbb4f40ea33d873deb4ce2
Coder <coder@mahinge.com>
2025-10-03 00:00:21 +0000

    dev options

--- astro.config.mjs
+++ astro.config.mjs
@@ -131,6 +131,13 @@ export default defineConfig({
   output: 'static',
 
   vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
     define: {
       global: 'globalThis',
     },
