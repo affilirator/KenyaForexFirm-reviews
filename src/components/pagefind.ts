de82d268f65d68947567cf4e0141c8a5533aaf53
default <coder@mahinge.com>
2025-10-06 04:16:16 +0000

    try news server

--- src/components/pagefind.ts
+++ src/components/pagefind.ts
@@ -2,7 +2,7 @@ import * as pagefind from 'pagefind';
 
 export const pagefindIndex = async () => {
   const { index } = await pagefind.createIndex({
-    rootSelector: 'html',
+   // rootSelector: 'html',
     excludeSelectors: ['.my-code-blocks'],
     forceLanguage: 'en',
     keepIndexUrl: false,
@@ -16,43 +16,15 @@ export const pagefindIndex = async () => {
 
   // Index all HTML files in a directory
   await index?.addDirectory({
-    path: './dist',
+    path: 'dist',
     glob: '**/*.{html}',
   });
 
   // Get the index files in-memory
-  const { files } = await index?.getFiles();
+  //const { files } = await index?.getFiles();
 
   // Or, write the index to disk
   await index?.writeFiles({
-    outputPath: 'public/pagefind',
+    outputPath: 'dist/pagefind',
   });
 };
-
-// Create a Pagefind search index to work with
-const { index } = await pagefind.createIndex();
-if (!index) {
-  throw new Error('Failed to create Pagefind index');
-}
-
-// Index all HTML files in a directory
-await index?.addDirectory({
-  path: './dist',
-});
-
-// Add extra content
-/*
-await index?.addCustomRecord({
-  url: '/resume.pdf',
-  content: 'Aenean lacinia bibendum nulla sed consectetur',
-  language: 'en',
-});
-*/
-
-// Get the index files in-memory
-const { files } = await index?.getFiles();
-
-// Or, write the index to disk
-await index?.writeFiles({
-  outputPath: 'public/pagefind',
-});
